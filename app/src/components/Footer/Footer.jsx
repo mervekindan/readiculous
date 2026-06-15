dd7987fa889456c12d5e7c725bf1fcf270b6ecd9
mervekindan <mervekanmaz10@gmail.com>
2026-06-15 14:09:05 +0200

    Refactor Footer, import SocialMediaItem and add basic styles

--- app/src/components/Footer/Footer.jsx
+++ app/src/components/Footer/Footer.jsx
@@ -1,7 +1,39 @@
+import { SocialMediaItem } from "../SocialMediaItem/SocialMediaItem";
+import "./Footer.css";
+
 export function Footer() {
     return (
-        <div className="footer">
-            <p>© 2026 Readiculous, All rights reserved.</p>
-        </div>
+        <footer className="footer">
+            <div className="footer-description">
+                <h3>Readiculous</h3>
+                <p>Read more. Become more.</p>
+                <p>&copy; 2026 Readiculous, All rights reserved.</p>
+            </div>
+            <div className="contact-us">
+                <h3>Contact Us</h3>
+                <p>FAQ</p>
+                <p>Help</p>
+            </div>
+            <div className="footer-links">
+                <h3>Follow Us</h3>
+                <ul className="footer-list">
+                    <SocialMediaItem
+                        url="https://facebook.com"
+                        title="Facebook"
+                        icon="../../public/socialmedia/Facebook.png"
+                    />
+                    <SocialMediaItem
+                        url="https://instagram.com"
+                        title="Instagram"
+                        icon="../../public/socialmedia/Instagram.png"
+                    />
+                    <SocialMediaItem
+                        url="https://linkedin.com"
+                        title="LinkedIn"
+                        icon="../../public/socialmedia/LinkedIn.png"
+                    />
+                </ul>
+            </div>
+        </footer>
     );
 }
