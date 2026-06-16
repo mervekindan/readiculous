200b9049dde4f7174ee002da96610b0515979c46
Anastasia Solodova <132252079+nastiasolo@users.noreply.github.com>
2026-06-16 09:57:35 +0200

    connected popups to layout

--- app/src/components/Layout/Layout.jsx
+++ app/src/components/Layout/Layout.jsx
@@ -1,10 +1,12 @@
 import { Outlet } from "react-router-dom";
 import { NavBar } from "../NavBar/NavBar.jsx";
 import { Footer } from "../Footer/Footer.jsx";
+import NotificationPopup from "../NotificationPopup/NotificationPopup";
 
 function Layout() {
   return (
     <div className="layout">
+      <NotificationPopup />
       <header>
         <NavBar />
       </header>
