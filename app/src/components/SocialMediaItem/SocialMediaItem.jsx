939b25231269ad292aa06fa8dea9a3694f579313
Anastasia Solodova <132252079+nastiasolo@users.noreply.github.com>
2026-06-27 11:40:53 +0200

    Footer: removed underlines for links.

--- app/src/components/SocialMediaItem/SocialMediaItem.jsx
+++ app/src/components/SocialMediaItem/SocialMediaItem.jsx
@@ -3,7 +3,7 @@ import "./SocialMediaItem.css";
 export function SocialMediaItem({ url, title, icon }) {
   return (
     <li className="item">
-      <a href={url} className="link">
+      <a href={url} className="footer-link">
         <img src={icon} alt={title} className="icon" />
         {title}
       </a>
