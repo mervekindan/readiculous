c7d6fa2b06a68e3e762de82f9d46ffb09429853b
Anastasia Solodova <132252079+nastiasolo@users.noreply.github.com>
2026-06-27 13:07:11 +0200

    social media links open in new browser window

--- app/src/components/SocialMediaItem/SocialMediaItem.jsx
+++ app/src/components/SocialMediaItem/SocialMediaItem.jsx
@@ -3,7 +3,12 @@ import "./SocialMediaItem.css";
 export function SocialMediaItem({ url, title, icon }) {
   return (
     <li className="item">
-      <a href={url} className="footer-link">
+      <a
+        href={url}
+        className="footer-link"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
         <img src={icon} alt={title} className="icon" />
         {title}
       </a>
